d59a2cd54fcb9d889f8f543f570298922c04e00c
Ivana Wang <ivanawan@usc.edu>
2015-02-27 15:04:39 -0800

    HTML pages
    HTML web pages + CSS styles

---
+++ Song List page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="pageStyles.css">
+    </head>
+
+<body bgcolor="#A4A4A4"> <!--gray background -->
+
+    <div id="songHeader">
+        <h1>LyricFloat</h1>
+        <h2> SEARCH WORD </h2> <!-- VARIABLE -->
+    </div>
+
+    <div id = "songTitles">
+        <div id="titles">
+        Song Title
+        </div>
+
+       <div id = "frequency">
+       Frequency
+       </div>
+    </div>
+
+    <div id="empty"></div>
+
+    <div id = "buttons">
+        <button id="backToCloud">
+            Back
+        </button>
+    </div>
+
+</body>
+</html>
---
+++ Word Cloud page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="pageStyles.css">
+</head>
+
+<body bgcolor="#A4A4A4"> <!--gray background -->
+
+    <div id="wordCloudHeader">
+        <h1>LyricFloat</h1>
+    </div>
+
+    <div id="wordCloud">
+        <img src="http://static.guim.co.uk/sys-images/Guardian/Pix/contributor/2009/12/9/1260365203581/Wordle-of-Alistair-Darili-002.jpg" alt="Mountain View" style="width:800px;height:600px">
+    </div>
+
+    <div id="empty"></div>
+
+    <div id="artistSearch">
+        <form id="search"><br>
+            <input type="text" name="firstname" placeholder="Enter an artist">
+            <br>
+        </form>
+    </div>
+
+    <div id = "buttons">
+        <button id="submit">
+            Submit
+        </button>
+        <button id="addToCloud">
+            Add to Cloud
+        </button>
+        <button id="fbShare">
+            Share
+        </button>
+    </div>
+
+</body>
+</html>
