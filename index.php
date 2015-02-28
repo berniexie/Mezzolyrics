a635bcb7ee38e563418e675f8a31de8893a430fd
Bernard Xie <bxie94@gmail.com>
2015-02-27 19:43:47 -0800

    Figured out templates

--- index.php
+++ index.php
@@ -7,40 +7,8 @@ $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
-$app->get('/mezzolyrics', function() use ($app) {
-	$template = <<<EOT
-<!DOCTYPE html>
-    <html>
-    <head>
-    <link rel="stylesheet" type="text/css" href="css/myStyles.css">
-    </head>
-
-  	<body bgcolor="#660099">
-
-	<div id="header">
-	<h1>Mezzolyrics</h1>
-	</div>
-
-	<div id="canvas">
-	<img src="http://static.guim.co.uk/sys-images/Guardian/Pix/contributor/2009/12/9/1260365203581/Wordle-of-Alistair-Darili-002.jpg" alt="Mountain View" style="width:800px;height:600px">
-	</div>
-	<div id="empty"></div>
-
-	<div id="wrapper">
-    <form id="search"><br>
-	<input type="text" name="firstname" placeholder="enter an artist">
-	<br></form>
-    <button id="second">Search!</button>
-	</div>
-
-	<div id="footer">
-	Copyright Mezzolyrics
-	</div>
-
-	</body> 
-   	</html>
-EOT;
-    echo $template;
+$app->get('/', function() use ($app) {
+  $app->render('word_cloud_page.php');
 });
 
 $app->run();
