adff5e56239b4d667eafa3a09be63053983b13e3
Jenny Yoshihara <jyoshiha@usc.edu>
2015-02-28 15:52:41 -0800

    Add class folder

---
+++ classes/Word.php
@@ -0,0 +1,20 @@
+<?php
+
+class Word
+{
+	private $word;	// string
+	private $songs;	// array of Song objects
+
+   	public function __construct($word, $songs)
+   	{
+   		$this->word = $word;
+   		$this->songs = $songs;
+   	}
+
+	public function getSongs()
+	{
+		return $songs;
+	}
+}
+
+?>
