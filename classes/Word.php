04300c8aca25033c1f063454a08689a080bc5baf
Bernard Xie <bxie94@gmail.com>
2015-02-28 17:06:28 -0800

    added frequency, commented out addnewwords

--- classes/Word.php
+++ classes/Word.php
@@ -4,11 +4,13 @@ class Word
 {
 	private $word;	// string
 	private $songs;	// array of Song objects
+        private $frequency; //frequency of that word in the wordCloud
 
-   	public function __construct($word, $songs)
+   	public function __construct($word, $songs, $frequency)
    	{
    		$this->word = $word;
    		$this->songs = $songs;
+                $this->frequency = $frequency;
    	}
 
 	public function getSongs()
@@ -17,6 +19,9 @@ class Word
 	}
         public function getString(){
                 return $word;
+        }
+        public function getFrequency(){
+                return $frequency;
         }
 }
 
