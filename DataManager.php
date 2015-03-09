afb78163de492aa44747aa9e1c7e80dc59ff992c
Jenny Yoshihara <jyoshiha@usc.edu>
2015-03-09 02:05:37 -0700

    Changed clearing of songs in getWords() and changed addArtist()

--- DataManager.php
+++ DataManager.php
@@ -12,54 +12,75 @@ class DataManager
   private $cloud;
   private $apiManager;
 
-  public function __construct(){
+  public function __construct()
+  {
     //the usefulness of this is debatable
     $this->apiManager = new APIManager();
   }
 
-  public function clearArtists(){
+  public function clearArtistsAndSongs()
+  {
     $this->artists = null;
+    $this->songs = null;
   }
 
-  public function getAutofillSuggestions($userInput){
+  public function getAutofillSuggestions($userInput)
+  {
+    // Passes autofill suggestions from apiManager
     return $this->apiManager->getArtistSuggestion($userInput);
   }
 
-  public function addArtist($artistName){
+  public function addArtist($artistName)
+  {
     $this->artists[] = $artistName;
     
-    // Get songs from new artist
+    $this->songs = $this->apiManager->getArtistSongs($artistName);
+
+    /*// Get songs from new artist
     $newSongs = array();
     $newSongs = $this->apiManager->getArtistSongs($artistName);
 
     // Add songs from new artist to songs array
-    $this->songs = array_merge($this->songs, $newSongs);
+    $this->songs = array_merge($this->songs, $newSongs);*/
   }
    
-  public function getWords(){
-    foreach($this->songs as $song){            
+  public function getWords()
+  {
+    foreach($this->songs as $song)
+    {            
       $lyrics = $song->getParsedLyrics(); //this gets the individual lyrics
-      foreach($lyrics as $lyric){
+      
+      foreach($lyrics as $lyric)
+      {
         $bool = false;
-        foreach($this->words as $word){
+
+        foreach($this->words as $word)
+        {
           //if it does, call the found() function in the word
-          if($lyric == $word->getContent()){
+          if($lyric == $word->getContent())
+          {
             $word->found($song);
             $bool = true; 
           }
         }
-        if($bool == false) {
+
+        if($bool == false)
+        {
           //If not, initialize it to a word object and add it to the array
           $word = new Word($lyric, $song);
           $this->words[] = $word;
         }
       }
-      unset($song); //this pops the item from the array so we don't have to parse it each time
+
+      //unset($song); //this pops the item from the array so we don't have to parse it each time
     }
+
+    $this->songs = null; // Set to null because unset was not removing item from array
   }
 
   //this is for when the addArtist button is clicked
-  public function getAddCloud($artistName){
+  public function getAddCloud($artistName)
+  {
     $this->addArtist($artistName);
     $this->getWords();
 
@@ -71,8 +92,9 @@ class DataManager
   }
 
   //this is for when the submit button is clicked
-  public function getSubmitCloud($artistName){
-    $this->clearArtists();
+  public function getSubmitCloud($artistName)
+  {
+    $this->clearArtistsAndSongs();
     return $this->getAddCloud($artistName);
   }
 }
